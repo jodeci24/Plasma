d30ba3af244df6c99f963f6a10c5005d1978dd91
Darryl Pogue <darryl@dpogue.ca>
2015-06-14 21:23:58 -0700

    Fix DXTDirectXCodec under MinGW.

--- Sources/Plasma/PubUtilLib/plGImage/hsDXTDirectXCodec.cpp
+++ Sources/Plasma/PubUtilLib/plGImage/hsDXTDirectXCodec.cpp
@@ -62,6 +62,18 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 #   include "hsWindows.h"
 #   include <ddraw.h>
 #   include <d3d9.h>
+
+#   ifndef FOURCC_DXT1
+#       define MAKEFOURCC(ch0, ch1, ch2, ch3)                              \
+                ((DWORD)(BYTE)(ch0) | ((DWORD)(BYTE)(ch1) << 8) |   \
+                ((DWORD)(BYTE)(ch2) << 16) | ((DWORD)(BYTE)(ch3) << 24 ))
+
+#       define FOURCC_DXT1  (MAKEFOURCC('D','X','T','1'))
+#       define FOURCC_DXT2  (MAKEFOURCC('D','X','T','2'))
+#       define FOURCC_DXT3  (MAKEFOURCC('D','X','T','3'))
+#       define FOURCC_DXT4  (MAKEFOURCC('D','X','T','4'))
+#       define FOURCC_DXT5  (MAKEFOURCC('D','X','T','5'))
+#   endif
 #endif
 
 #include "hsDXTDirectXCodec.h"
